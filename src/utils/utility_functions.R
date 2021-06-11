d3b9bc1076e7a440261477af78ffe514b5c68176
hansvancalster <hans.vancalster@inbo.be>
2021-06-11 11:10:41 +0200

    fix code style

--- src/utils/utility_functions.R
+++ src/utils/utility_functions.R
@@ -15,10 +15,12 @@ render_report <- function(...) {
 get_map_data_moneos <- function() {
   if (Sys.getenv("MAP_DATA_MONEOS") == "") {
     usethis::edit_r_environ("project")
-    stop(paste0("No MAP_DATA_MONEOS environmental variable found, ",
-                "please edit the .Renviron file and add a line ",
-                "MAP_DATA_MONEOS = '<path to the team drive procesbeheer folder>' ",
-                "and restart R after you have done this."))
+    stop(
+      paste0(
+        "No MAP_DATA_MONEOS environmental variable found, ",
+        "please edit the .Renviron file and add a line ",
+        "MAP_DATA_MONEOS = '<path to the team drive procesbeheer folder>' ",
+        "and restart R after you have done this."))
   }
   path <- Sys.getenv("MAP_DATA_MONEOS")
   return(path)

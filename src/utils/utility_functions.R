b9ab197be129d46dadcf74eb4caf2c86ef6429f3
hansvancalster <hans.vancalster@inbo.be>
2021-06-11 10:21:11 +0200

    function to render report

--- src/utils/utility_functions.R
+++ src/utils/utility_functions.R
@@ -2,3 +2,11 @@ find_file <- function(...) {
   rprojroot::find_root_file(...,
                             criterion = rprojroot::is_git_root)
 }
+
+
+render_report <- function(...) {
+  oldwd <- getwd()
+  setwd(find_file("src", "rapport"))
+  bookdown::render_book(...)
+  setwd(oldwd)
+}

d977bc418443ae6aa21575b614312f3a46fea1d4
CARMEN <raisa.carmen@inbo.be>
2021-10-11 16:00:40 +0200

    benthos & metadata data cleaning

--- src/scripts/test_inlezen_tabellen.R
+++ src/scripts/test_inlezen_tabellen.R
@@ -2,6 +2,7 @@ library(readxl)
 library(rprojroot)
 library(fs)
 library(purrr)
+library(tidyverse)
 source(find_root_file("src/utils/utility_functions.R",
                       criterion = is_git_root))
 
@@ -18,3 +19,33 @@ load_excel <- function(path) {
 }
 
 walk(paths, load_excel)
+
+sp2018_metadata %>%
+  rename(ecotoop_gepland = `ecotoop gepland`) %>%
+  mutate(year = 2018) -> sp2018_metadata
+sp2019_metadata %>%
+  rename(datum = Datum) %>%
+  mutate(year = 2019,
+         ecotoop_gepland = NA) %>%
+  bind_rows(sp2018_metadata) %>%
+  mutate(campagne = as.factor(campagne),
+         staalcode = as.factor(substr(staal,1,nchar(staal)-3)),#lijkt steeds een code en daarna een volgnummer
+         tidaal = as.factor(tidaal),
+         geomorf = as.factor(geomorf),
+         ecotoop_gepland = as.factor(ecotoop_gepland),
+         ecotoop_werkelijk = as.factor(ecotoop_gepland),
+         SalZone = as.factor(SalZone),
+         Vallei_deel = as.factor(Vallei_deel),
+         Omessegment = as.factor(Omessegment),
+         KRWzone = as.factor(KRWzone),
+         year = as.factor(year)) -> metadata
+sp2018_benthos %>%
+  mutate(year = 2018) ->sp2018_benthos
+sp2019_benthos %>%
+  mutate(year = 2019) %>%
+  bind_rows(sp2018_benthos) %>%
+  mutate(staalcode = as.factor(substr(staal,1,nchar(staal)-3)),
+         soort = as.factor(soort)) -> benthos
+rm(sp2018_benthos, sp2019_benthos, sp2018_metadata, sp2019_metadata)
+
+

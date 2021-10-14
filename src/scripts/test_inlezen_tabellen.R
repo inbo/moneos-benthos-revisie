56e93fdcbbd07f3cfbf388b4db70334775d09d1b
CARMEN <raisa.carmen@inbo.be>
2021-10-14 09:10:48 +0200

    Check coding style

--- src/scripts/test_inlezen_tabellen.R
+++ src/scripts/test_inlezen_tabellen.R
@@ -4,7 +4,8 @@ library(fs)
 library(purrr)
 library(tidyverse)
 source(find_root_file("src/utils/utility_functions.R",
-                      criterion = is_git_root))
+  criterion = is_git_root
+))
 
 data_path <- get_map_data_moneos()
 
@@ -25,34 +26,43 @@ sp2018_metadata %>%
   mutate(jaar = 2018) -> sp2018_metadata
 sp2019_metadata %>%
   rename(datum = Datum) %>%
-  mutate(jaar = 2019,
-         ecotoop_gepland = NA) %>%
+  mutate(
+    jaar = 2019,
+    ecotoop_gepland = NA
+  ) %>%
   bind_rows(sp2018_metadata) %>%
-  mutate(campagne = as.factor(campagne),
-         staalcode = as.factor(substr(staal,1,nchar(staal)-3)),#lijkt steeds een code en daarna een volgnummer
-         tidaal = as.factor(tidaal),
-         geomorf = as.factor(geomorf),
-         ecotoop_gepland = as.factor(ecotoop_gepland),
-         ecotoop_werkelijk = as.factor(ecotoop_werkelijk),
-         SalZone = as.factor(SalZone),
-         Vallei_deel = as.factor(Vallei_deel),
-         Omessegment = as.factor(Omessegment),
-         KRWzone = as.factor(KRWzone),
-         jaar = as.factor(jaar)) -> metadata
+  mutate(
+    campagne = as.factor(campagne),
+    staalcode = as.factor(substr(staal, 1, nchar(staal) - 3)),
+    tidaal = as.factor(tidaal),
+    geomorf = as.factor(geomorf),
+    ecotoop_gepland = as.factor(ecotoop_gepland),
+    ecotoop_werkelijk = as.factor(ecotoop_werkelijk),
+    SalZone = as.factor(SalZone),
+    Vallei_deel = as.factor(Vallei_deel),
+    Omessegment = as.factor(Omessegment),
+    KRWzone = as.factor(KRWzone),
+    jaar = as.factor(jaar)
+  ) -> metadata
 sp2018_benthos %>%
-  mutate(jaar = 2018) ->sp2018_benthos
+  mutate(jaar = 2018) -> sp2018_benthos
 sp2019_benthos %>%
   mutate(jaar = 2019) %>%
   bind_rows(sp2018_benthos) %>%
-  mutate(staalcode = as.factor(substr(staal,1,nchar(staal)-3)),
-         soort = as.factor(soort),
-         jaar = as.factor(jaar)) -> benthos
+  mutate(
+    staalcode = as.factor(substr(staal, 1, nchar(staal) - 3)),
+    soort = as.factor(soort),
+    jaar = as.factor(jaar)
+  ) -> benthos
 rm(sp2018_benthos, sp2019_benthos, sp2018_metadata, sp2019_metadata)
-
-#check voor dubbels in the metadata
-a <- which(duplicated(metadata[,c("staal", "staalcode", "jaar")]))
-a <- c(a, which(duplicated(metadata[,c("staal", "staalcode", "jaar")], fromLast=T)))
-#Staal DI_SP_04 staat voor beide jaren twee keer in de metadata met lichtjes andere X-Y coordinaten en ander ecotoop_werkelijk. Dit dient uitgeklaard te worden want anders krijgen we dubbels in onderstaande left join.
-
-benthos %>% left_join(metadata, by = c("staal", "staalcode", "jaar", "datum")) -> benthos
-
+# check voor dubbels in the metadata
+a <- which(duplicated(metadata[, c("staal", "staalcode", "jaar")]))
+a <- c(a, which(duplicated(metadata[, c("staal", "staalcode", "jaar")],
+  fromLast = TRUE
+)))
+# Staal DI_SP_04 staat voor beide jaren twee keer in de metadata met lichtjes
+# andere X-Y coordinaten en ander ecotoop_werkelijk. Dit dient uitgeklaard te
+# worden want anders krijgen we dubbels in onderstaande left join.
+benthos %>%
+  left_join(metadata, by = c("staal", "staalcode", "jaar", "datum")) ->
+benthos
